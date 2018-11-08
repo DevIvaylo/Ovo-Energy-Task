44ed3dc103f77b3de5e480aec6696fb6b7f88df5
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-11-08 14:58:23 +0000

    updating the root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RockPaperScissorsComponent } from './rock-paper-scissors/rock-paper-scissors.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RockPaperScissorsComponent,
   ],
   imports: [
     BrowserModule
