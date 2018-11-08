2c51c15f2fb97b455c10dd961a5541cb9e167544
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-11-08 22:12:16 +0000

    adding tests for calculator service

--- src/app/services/winning-player-calculator.service.spec.ts
+++ src/app/services/winning-player-calculator.service.spec.ts
@@ -1,15 +1,111 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { WinningPlayerCalculatorService } from './winning-player-calculator.service';
+import {TestBed, inject, async} from '@angular/core/testing';
+import {WinningPlayerCalculatorService} from './winning-player-calculator.service';
+import {ItemTypes} from '../classes/item-types';
+import {WinningPlayer} from '../interfaces/winning-player';
 
 describe('WinningPlayerCalculatorService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [WinningPlayerCalculatorService]
-    });
-  });
+
+  beforeEach(async(
+    () => {
+      TestBed.configureTestingModule({
+        imports: [],
+        providers: [
+          WinningPlayerCalculatorService,
+        ],
+      });
+    }));
+
+  const getWinningPlayer = (winningPlayerLabel: string,
+                            playerPointCount?: number,
+                            computerPointCount?: number): WinningPlayer => {
+    return {
+      winningPlayerLabel: winningPlayerLabel,
+      playerPointCount: playerPointCount,
+      computerPointCount: computerPointCount
+    };
+  };
+
+  const computerPlayerWinLabel = 'Computer wins!!';
+  const playerWinLabel = 'Player wins!!';
+  const playerDrawLabel = 'Its a Draw!!';
 
   it('should be created', inject([WinningPlayerCalculatorService], (service: WinningPlayerCalculatorService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('getWinningPlayer should calculate a draw when both players have selected Rock',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.rockItemType, ItemTypes.rockItemType)
+        ).toEqual(getWinningPlayer(playerDrawLabel, 0, 0));
+      })
+    ));
+
+  it('getWinningPlayer should calculate a draw when both players have selected Paper',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.paperItemType, ItemTypes.paperItemType)
+        ).toEqual(getWinningPlayer(playerDrawLabel, 0, 0));
+      })
+    ));
+
+  it('getWinningPlayer should calculate a draw when both players have selected Scissors',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.scissorsItemType, ItemTypes.scissorsItemType)
+        ).toEqual(getWinningPlayer(playerDrawLabel, 0, 0));
+      })
+    ));
+
+  it('getWinningPlayer should calculate a computer win when player selected Scissors and computer selects Rock',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.scissorsItemType, ItemTypes.rockItemType)
+        ).toEqual(getWinningPlayer(computerPlayerWinLabel, 0, 1));
+      })
+    ));
+
+  it('getWinningPlayer should calculate a player win when player selected Rock and computer selects Scissors',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.rockItemType, ItemTypes.scissorsItemType)
+        ).toEqual(getWinningPlayer(playerWinLabel, 1, 0));
+      })
+    ));
+
+
+  it('getWinningPlayer should calculate a player win when player selected Scissors and computer selects Paper',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer
+          (ItemTypes.scissorsItemType, ItemTypes.paperItemType)
+        ).toEqual(getWinningPlayer(playerWinLabel, 1, 0));
+      })
+    ));
+
+
+  it('getWinningPlayer should calculate a computer win when player selected Paper and computer selects Scissors',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.paperItemType, ItemTypes.scissorsItemType)
+        ).toEqual(getWinningPlayer(computerPlayerWinLabel, 0, 1));
+      })
+    ));
+
+
+  it('getWinningPlayer should calculate a player win when player selected Paper and computer selects Rock',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.paperItemType, ItemTypes.rockItemType)
+        ).toEqual(getWinningPlayer(playerWinLabel, 1, 0));
+      })
+    ));
+
+  it('getWinningPlayer should calculate a computer win when player selected Rock and computer selects Paper',
+    async(inject([WinningPlayerCalculatorService], (winningPlayerCalculatorService: WinningPlayerCalculatorService) => {
+        expect(winningPlayerCalculatorService.getWinningPlayer(
+          ItemTypes.rockItemType, ItemTypes.paperItemType)
+        ).toEqual(getWinningPlayer(computerPlayerWinLabel, 0, 1));
+      })
+    ));
 });
+
