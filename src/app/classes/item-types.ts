176de84ac1f556fec60cb299364f6af3c8419cbe
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-11-08 14:51:15 +0000

    adding class for item types

---
+++ src/app/classes/item-types.ts
@@ -0,0 +1,9 @@
+import {Item} from '../interfaces/item';
+
+export class ItemTypes {
+  public static readonly rockItemType: Item = {name: 'Rock', value: 1};
+  public static readonly paperItemType: Item = {name: 'Paper', value: 2};
+  public static readonly scissorsItemType: Item = {name: 'Scissors', value: 3};
+
+}
+
