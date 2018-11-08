5e0a45c387c91069c9f9069b708b183f394dc7be
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-11-08 19:52:56 +0000

    some more tests

--- src/app/rock-paper-scissors/rock-paper-scissors.component.spec.ts
+++ src/app/rock-paper-scissors/rock-paper-scissors.component.spec.ts
@@ -70,5 +70,214 @@ describe('RockPaperScissorsComponent', () => {
       expect(computerPoints.nativeElement.textContent).toEqual('Computer score: 1');
     })
   ));
+
+
+  it('when player selects paper and computer selects rock, player wins', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.rockItemType;
+        });
+
+      paperButton.nativeElement.click();
+      fixture.detectChanges();
+
+      paperButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Player wins!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Paper');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Rock');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledTimes(2);
+      expect(playerPoints.nativeElement.textContent).toEqual('Player score: 2');
+    })
+  ));
+
+  it('when player selects scissors and computer selects rock, computer wins', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.rockItemType;
+        });
+
+      scissorsButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Computer wins!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Scissors');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Rock');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+      expect(computerPoints.nativeElement.textContent).toEqual('Computer score: 1');
+    })
+  ));
+
+  it('when player selects rock and computer selects scissors, player wins', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.scissorsItemType;
+        });
+
+      rockButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Player wins!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Rock');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Scissors');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+    })
+  ));
+
+  it('when player selects paper and computer selects scissors, computer wins', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.scissorsItemType;
+        });
+
+      paperButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Computer wins!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Paper');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Scissors');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+      expect(playerPoints.nativeElement.textContent).toEqual('Player score: 0');
+      expect(computerPoints.nativeElement.textContent).toEqual('Computer score: 1');
+    })
+  ));
+
+  it('when player 1 selects scissors and computer selects paper, player wins', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.paperItemType;
+        });
+
+      scissorsButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Player wins!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Scissors');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Paper');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+      expect(playerPoints.nativeElement.textContent).toEqual('Player score: 1');
+    })
+  ));
+
+  it('when player 1 selects scissors and computer selects scissors, nobody wins, its a draw', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.scissorsItemType;
+        });
+
+      scissorsButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Its a Draw!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Scissors');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Scissors');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+      expect(playerPoints.nativeElement.textContent).toEqual('Player score: 0');
+      expect(computerPoints.nativeElement.textContent).toEqual('Computer score: 0');
+    })
+  ));
+
+  it('when player selects rock and computer selects rock, nobody wins, its a draw', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.rockItemType;
+        });
+
+      rockButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Its a Draw!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Rock');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Rock');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+      expect(playerPoints.nativeElement.textContent).toEqual('Player score: 0');
+      expect(computerPoints.nativeElement.textContent).toEqual('Computer score: 0');
+    })
+  ));
+
+  it('when player selects paper and computer selects paper, nobody wins, its a draw', async(inject(
+    [RandomItemGeneratorService],
+    (randomItemGeneratorService: RandomItemGeneratorService) => {
+      component.ngOnInit();
+
+      spyOn(randomItemGeneratorService, 'getRandomItem').and.callFake(
+        () => {
+          return ItemTypes.paperItemType;
+        });
+
+      paperButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const winner = fixture.debugElement.query(By.css('#winner'));
+      const playerSelection = fixture.debugElement.query(By.css('#playerSelection'));
+      const computerSelection = fixture.debugElement.query(By.css('#computerSelection'));
+      expect(winner.nativeElement.textContent).toEqual('Its a Draw!!');
+      expect(playerSelection.nativeElement.textContent).toEqual('Player choice: Paper');
+      expect(computerSelection.nativeElement.textContent).toEqual('Computer choice: Paper');
+      expect(randomItemGeneratorService.getRandomItem)
+        .toHaveBeenCalledWith(component.playerSelectionOptions);
+      expect(playerPoints.nativeElement.textContent).toEqual('Player score: 0');
+      expect(computerPoints.nativeElement.textContent).toEqual('Computer score: 0');
+    })
+  ));
 });
 
